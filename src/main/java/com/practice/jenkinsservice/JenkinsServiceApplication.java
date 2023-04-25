da084c70278f3a8ad6d773d48ebe39ac6fc58a4f
mohit suryawanshi <mohit.suryawanshi@globallogic.com>
2023-04-25 20:47:33 +0530

    Deploying war type file to external tomcat

--- src/main/java/com/practice/jenkinsservice/JenkinsServiceApplication.java
+++ src/main/java/com/practice/jenkinsservice/JenkinsServiceApplication.java
@@ -2,13 +2,22 @@ package com.practice.jenkinsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JenkinsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsServiceApplication.class, args);
 		System.out.println("Hi I am jenkins application");
+	}
+
+	@GetMapping("/deploy")
+	public String deploy()
+	{
+		return "Deploying with jenkins on external tomcat is done";
 	}
 
 }
