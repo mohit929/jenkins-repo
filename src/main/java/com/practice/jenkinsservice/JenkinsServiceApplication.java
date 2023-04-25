950f82485645763e55c35d76cc911c5bf514c4b7
mohit suryawanshi <mohit.suryawanshi@globallogic.com>
2023-04-25 13:17:44 +0530

    Final Working commit

--- src/main/java/com/practice/jenkinsservice/JenkinsServiceApplication.java
+++ src/main/java/com/practice/jenkinsservice/JenkinsServiceApplication.java
@@ -9,7 +9,6 @@ public class JenkinsServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsServiceApplication.class, args);
 		System.out.println("Hi I am jenkins application");
-		System.out.println("Hello I am mohit suryawanshi")
 	}
 
 }
